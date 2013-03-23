f2469a9cddcfee7120ecd223c1bec2710643ab85
fmuhtadin <fmuhtadin@gmail.com>
2013-03-24 01:51:21 +0700

    Initialize Project

---
+++ gunbond/PeerModule/GunbondPeer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets; 
+
+namespace PeerModule
+{
+    class GunbondPeer
+    {
+        SocketPermission permission;
+        Socket socketListener;
+        IPHostEntry ipHost;
+        IPAddress ipAddr;
+        IPEndPoint ipEndPoint;
+
+        public GunbondPeer()
+        {
+            permission = new SocketPermission(NetworkAccess.Accept, TransportType.Tcp, "", SocketPermission.AllPorts);
+            
+            ipHost = Dns.GetHostEntry("");
+            ipAddr = ipHost.AddressList[0];
+            ipEndPoint = new IPEndPoint(ipAddr, 4510);
+
+            socketListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void JoinRoom()
+        {
+        }
+
+        public void InitiateHandshake()
+        {
+        }
+
+        public void GetListRoom()
+        {
+        }
+
+        public void StartGame()
+        {
+        }
+
+        public void QuitRoom()
+        {
+        }
+
+    }
+}
---
+++ gunbond/TrackerModule/GunbondTracker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets; 
+
+namespace TrackerModule
+{
+    class GunbondTracker
+    {
+        SocketPermission permission;
+        Socket sListener;
+        IPHostEntry ipHost;
+        IPAddress ipAddr;
+        IPEndPoint ipEndPoint;
+        
+        public GunbondTracker()
+        {
+            permission = new SocketPermission(NetworkAccess.Accept, TransportType.Tcp, "", SocketPermission.AllPorts);
+            
+            ipHost = Dns.GetHostEntry("");
+            ipAddr = ipHost.AddressList[0];
+            ipEndPoint = new IPEndPoint(ipAddr, 4510);
+
+            sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void SetMaxPeer()
+        {
+        }
+
+        public void SetMaxRoom()
+        {
+        }
+
+        public void LogOn()
+        {
+        }
+
+        public void LogOff()
+        {
+        }
+
+        public void Shutdown()
+        {
+        }
+    }
+}
